fa25f1c809494f11a7d17a19ebf1b29db95857f4
Petr Kushnir <peterkushnir@gmail.com>
2023-06-10 19:30:58 +0200

    Updated working bot

--- bot.py
+++ bot.py
@@ -1,32 +1,16 @@
-#!/usr/bin/env python
-# pylint: disable=unused-argument, wrong-import-position
-# This program is dedicated to the public domain under the CC0 license.
-
-"""
-Simple Bot to reply to Telegram messages.
-
-First, a few handler functions are defined. Then, those functions are passed to
-the Application and registered at their respective places.
-Then, the bot is started and runs until we press Ctrl-C on the command line.
-
-Usage:
-Basic Echobot example, repeats messages.
-Press Ctrl-C on the command line or send a signal to the process to stop the
-bot.
-"""
-
 import logging
-import os  # Import os module
-from dotenv import load_dotenv  # Import load_dotenv function
+import os
+from dotenv import load_dotenv
+import gpt4all
 
-load_dotenv()  # Load the environment variables
+load_dotenv()
 
 from telegram import __version__ as TG_VER
 
 try:
     from telegram import __version_info__
 except ImportError:
-    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
+    __version_info__ = (0, 0, 0, 0, 0)
 
 if __version_info__ < (20, 0, 0, "alpha", 1):
     raise RuntimeError(
@@ -37,51 +21,40 @@ if __version_info__ < (20, 0, 0, "alpha", 1):
 from telegram import ForceReply, Update
 from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters
 
-# Enable logging
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 logger = logging.getLogger(__name__)
 
+MODEL_NAME = os.getenv('MODEL_NAME', 'ggml-gpt4all-j-v1.3-groovy')
+gptj = gpt4all.GPT4All(MODEL_NAME)
 
-# Define a few command handlers. These usually take the two arguments update and
-# context.
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /start is issued."""
     user = update.effective_user
     await update.message.reply_html(
         rf"Hi {user.mention_html()}!",
         reply_markup=ForceReply(selective=True),
     )
 
-
 async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Send a message when the command /help is issued."""
     await update.message.reply_text("Help!")
 
-
-async def echo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Echo the user message."""
-    await update.message.reply_text(update.message.text)
-
+async def respond(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_input = update.message.text
+    messages = [{"role": "user", "content": user_input}]
+    response = gptj.chat_completion(messages, verbose=False)
+    await update.message.reply_text(response['choices'][0]['message']['content'])
 
 def main() -> None:
-    """Start the bot."""
-    # Create the Application and pass it your bot's token.
-    # Fetch the token from environment variables
     token = os.getenv('TELEGRAM_TOKEN')
     application = Application.builder().token(token).build()
 
-    # on different commands - answer in Telegram
     application.add_handler(CommandHandler("start", start))
     application.add_handler(CommandHandler("help", help_command))
 
-    # on non command i.e message - echo the message on Telegram
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, echo))
+    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, respond))
 
-    # Run the bot until the user presses Ctrl-C
     application.run_polling()
-
 
 if __name__ == "__main__":
     main()
--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import gpt4all
 gptj = gpt4all.GPT4All("ggml-gpt4all-j-v1.3-groovy")
-messages = [{"role": "user", "content": "Name 3 colors"}]
+messages = [{"role": "user", "content": "Что такое луна?"}]
 response = gptj.chat_completion(messages, verbose=False)
 print(response['choices'][0]['message']['content'])
